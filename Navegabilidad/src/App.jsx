1f65a15ed644e32e283de4cc98efcd546506ab88
Manuel <20220416@ricaldone.edu.sv>
2025-03-26 08:11:07 -0600

    bue

--- Navegabilidad/src/App.jsx
+++ Navegabilidad/src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home'
-import Inicio from '../pages/Inicio'
-import About from '../pages/About'
-import Calculator from '../pages/Calculator'
-import Components from '../pages/Components/Na'
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+
+import Inicio from '../src/Pages/Inicio'
+import About from '../src/Pages/About'
+import Calculator from '../src/Pages/Calculator'
+import Components from '../src/Pages/Components'
 import Nav from "./Components/Nav" 
 
 
@@ -17,8 +17,7 @@ function App() {
       <Nav />
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/inicio" element={<Inicio />} />
+          <Route path="/" element={<Inicio />} />
           <Route path="/about" element={<About />} />
           <Route path="/calculator" element={<Calculator />} />
           <Route path="/components" element={<Components />} />
